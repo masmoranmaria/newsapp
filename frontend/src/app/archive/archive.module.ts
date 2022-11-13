3eeafe53cc5f83187eb8772938f14767cd588f90
masmoranmaria <masmoranmaria@gmail.com>
2022-11-13 11:52:15 +0100

    feat: created archive module and component

---
+++ frontend/src/app/archive/archive.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ArchiveComponent } from './archive/archive.component';
+
+
+
+@NgModule({
+  declarations: [
+    ArchiveComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class ArchiveModule { }
