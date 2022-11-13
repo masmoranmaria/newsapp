e6b2029aaef0a4d29039b6890c1ecd2912cc09d4
masmoranmaria <masmoranmaria@gmail.com>
2022-11-13 12:00:29 +0100

    feat: added card to archive view

--- frontend/src/app/archive/archive.module.ts
+++ frontend/src/app/archive/archive.module.ts
@@ -1,15 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ArchiveComponent } from './archive/archive.component';
-
-
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   declarations: [
     ArchiveComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    SharedModule
   ]
 })
+
 export class ArchiveModule { }
--- frontend/src/app/archive/archive/archive.component.ts
+++ frontend/src/app/archive/archive/archive.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { NewsService } from 'src/app/services/news.service';
 
 @Component({
   selector: 'app-archive',
-  templateUrl: './archive.component.html',
-  styleUrls: ['./archive.component.css']
+  templateUrl: './archive.component.html'
 })
+
 export class ArchiveComponent implements OnInit {
 
-  constructor() { }
+  archiveData: any;
+
+  constructor(private archive: NewsService) { }
 
   ngOnInit(): void {
+    this.archive.getNews().subscribe((data) => this.archiveData = data);
   }
 
 }
